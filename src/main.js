402ca176727b73129cc7a3a75feca262b6d0494e
Roman Pogorelov <rmk140987@gmail.com>
2021-01-19 09:32:00 +0300

    2. Refactored user-info rank template.

--- src/main.js
+++ src/main.js
@@ -20,7 +20,12 @@ const main = document.querySelector(`.main`);
 const footer = document.querySelector(`.footer`);
 const footerStats = footer.querySelector(`.footer__statistics`);
 
-render(header, createUserInfoTemplate(), PlaceType.BEFORE_END);
+const filmsMocks = [];
+for (let i = 0; i < 25; i++) {
+  filmsMocks.push(generateFilm());
+}
+
+render(header, createUserInfoTemplate(filmsMocks), PlaceType.BEFORE_END);
 
 render(main, createMenuTemplate(), PlaceType.AFTER_BEGIN);
 render(main, createSortTemplate(), PlaceType.BEFORE_END);
@@ -30,11 +35,6 @@ render(main, createFilmsSection(), PlaceType.BEFORE_END);
 const films = main.querySelector(`.films`);
 const filmsList = films.querySelector(`.films-list`);
 render(filmsList, createFilmsContainer(), PlaceType.AFTER_BEGIN);
-
-const filmsMocks = [];
-for (let i = 0; i < 25; i++) {
-  filmsMocks.push(generateFilm());
-}
 
 const filmsContainer = main.querySelector(`.films-list__container`);
 for (let i = 0; i < CARD_QUANTITY; i++) {
--- src/view/user-info.js
+++ src/view/user-info.js
@@ -1,6 +1,23 @@
-const createUserInfoTemplate = () => {
+const getUserRank = (watchedQuantity) => {
+  let rank = ``;
+  if (watchedQuantity > 1 && watchedQuantity <= 10) {
+    rank = `novice`;
+  } else if (watchedQuantity >= 11 && watchedQuantity <= 20) {
+    rank = `fan`;
+  } else if (watchedQuantity >= 21) {
+    rank = `movie buff`;
+  }
+  return rank;
+};
+
+const createUserInfoTemplate = (films) => {
+  const watchedQuantity = films.reduce((accumulator, curFilm) => {
+    return accumulator + +curFilm.userDetails.alreadyWatched;
+  }, 0);
+  const rank = getUserRank(watchedQuantity);
+
   return `<section class="header__profile profile">
-            <p class="profile__rating">Movie Buff</p>
+            <p class="profile__rating">${rank}</p>
             <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
           </section>`;
 };
