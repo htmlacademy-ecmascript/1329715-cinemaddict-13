fd7fa187e3cb442c007a79d052696521d4f1cdc4
Roman Pogorelov <rmk140987@gmail.com>
2021-01-22 09:32:49 +0300

    6. Added Smart component.

---
+++ src/view/smart.js
@@ -0,0 +1,31 @@
+import {AbstractView} from "./abstract-view";
+
+class Smart extends AbstractView {
+  constructor(state) {
+    super();
+
+    this._state = state;
+  }
+
+  updateState(newState, isReload) {
+    this._state = Object.assign({}, this._state, newState);
+    if (isReload) {
+      this.updateElement();
+    }
+  }
+
+  updateElement() {
+    const oldElement = this.element;
+    const parent = oldElement.parentElement;
+    this.removeElement();
+    const newElement = this.element;
+    parent.replaceChild(newElement, oldElement);
+    this.restoreHandlers();
+  }
+
+  restoreHandlers() {
+    throw new Error(`Must be implemented`);
+  }
+}
+
+export {Smart};
