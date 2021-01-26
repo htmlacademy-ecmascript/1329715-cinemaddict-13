c759bbb8e8da069e622d957149c6d704bcd77c2d
Roman Pogorelov <rmk140987@gmail.com>
2021-01-26 07:38:52 +0300

    Fixed sort by date.

--- src/util/sort.js
+++ src/util/sort.js
@@ -9,6 +9,6 @@ const sort = {};
 sort[SortType.DEFAULT] = (films) => films;
 sort[SortType.RATED] = (films) => films.slice().sort((a, b) => b.filmInfo.rating - a.filmInfo.rating);
 sort[SortType.COMMENTED] = (films) => films.slice().sort((a, b) => b.comments.length - a.comments.length);
-sort[SortType.RELEASE_DATE] = (films) => films.slice().sort((a, b) => b.filmInfo.release.date - a.filmInfo.release.date);
+sort[SortType.RELEASE_DATE] = (films) => films.slice().sort((a, b) => new Date(b.filmInfo.release.date) - new Date(a.filmInfo.release.date));
 
 export {sort, SortType};
